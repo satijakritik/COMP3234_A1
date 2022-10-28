af83f7ab2d93a539e284ad093ebf617e34adb4ba
Kritik Satija <kritik.satija@neurallab.io>
2022-10-29 01:37:18 +0800

    Implemented the /exit command

--- GameClient.py
+++ GameClient.py
@@ -5,22 +5,24 @@ import sys
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST_NAME = sys.argv[1] #Parameter 1
+host_name = sys.argv[1] #Parameter 1
+server_port = int(sys.argv[2])
 
-if sys.argv[2].isdigit():
-    SERVER_PORT = int(sys.argv[2]) #Parameter 2
-else:
-    print("Error: Server's listening port is not an integer")
-    exit(1)
+# if sys.argv[2].isdigit():
+#     SERVER_PORT = int(sys.argv[2]) #Parameter 2
+# else:
+#     print("Error: Server's listening port is not an integer")
+#     exit(1)
 
-clientSocket.connect( (HOST_NAME, SERVER_PORT) )
+clientSocket.connect( (host_name, server_port) )
 
 auth = False
+in_game_hall = False
 
 while auth == False:
 # Get input for sending
-    user_name = input("Please input your username: ")
-    password = input("Please input your password: ")
+    user_name = input("Please input your username:\n")
+    password = input("Please input your password:\n")
 
     msg = f"/login {user_name} {password}"
 
@@ -30,8 +32,22 @@ while auth == False:
     
     if response.decode() == "1001 Authentication successful":
         auth = True
+        in_game_hall = True
     
     print(response.decode())
+    
+while in_game_hall:
+    cmd = input("")
+    clientSocket.send(cmd.encode())
+    
+    response = clientSocket.recv(1024)
+    
+    if response.decode() == "4001 Bye bye":
+        in_game_hall = False
+        clientSocket.close()
+        exit(1)
+    
+
     
     
 
--- GameServer.py
+++ GameServer.py
@@ -4,42 +4,70 @@ import socket
 import threading
 import sys
 
-if sys.argv[1].isdigit():
-    SERVER_PORT = int(sys.argv[1]) #Parameter 1
-else:
-    print("Error: Listening port is not an integer")
-    exit(1)
-FILE_PATH = sys.argv[2] #Parameter 2
+#-----Global variables----#
+STATES = {0: "OUT_OF_HOUSE", 1: "IN_THE_GAME_HALL", 2: "WAITING_IN_ROOM", 3: "PLAYING_A_GAME"}
+NUM_OF_ROOMS = 5
 
-print(f"server port = {SERVER_PORT}, file path = {FILE_PATH}")
+server_port = int(sys.argv[1]) #Parameter 1
+file_path = sys.argv[2] #Parameter 2
 
-test = "/login kritik 11"
-    
-success = "1001 Authentication successful"
-failure = "1002 Authentication failed"
+#-------------------------
+
+# if sys.argv[1].isdigit():
+#     server_port = int(sys.argv[1]) #Parameter 1
+# else:
+#     print("Error: Listening port is not an integer")
+#     exit(1)
+
+print(f"server port = {server_port}, file path = {file_path}")
 
 def thd_func(client):
     connectionSocket, addr = client    
     msg = connectionSocket.recv(1024)
     msg = msg.decode()
     
+    user_state = 0 #OUT_OF_HOUSE
+    
+    test = "/login kritik 11"
+    success = "1001 Authentication successful"
+    failure = "1002 Authentication failed"
+    
     while msg != test:
     
-        print(msg)
+        # print(msg)
         
         connectionSocket.send(failure.encode())
         
         msg = connectionSocket.recv(1024)
         msg = msg.decode()
-        # connectionSocket.close()
         
     connectionSocket.send(success.encode())
+    
+    user_state = 1 #IN_THE_GAME_HALL
+    
+    # cmd_list = ["\list", "\enter", "\exit"]
+    
+    msg = connectionSocket.recv(1024)
+    msg = msg.decode()
+    
+    exit = "4001 Bye bye"
+    
+    while msg != "/exit": #exit
         
+        print(msg)
+        
+        connectionSocket.send(msg.encode())
+        
+        msg = connectionSocket.recv(1024)
+        msg = msg.decode()
+        
+    connectionSocket.send(exit.encode())
+    
     connectionSocket.close()
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serverSocket.bind( ("", SERVER_PORT) )
+serverSocket.bind( ("", server_port) )
 
 serverSocket.listen(5)
 
